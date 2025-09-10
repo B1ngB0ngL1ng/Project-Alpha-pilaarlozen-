30f15a4a657c9788d1d0b5b8e5d922462a562120
JesseBr27 <1105432@hr.nl>
2025-09-10 09:00:48 +0200

    Update Location.cs

--- project a/Location.cs
+++ project a/Location.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Location
+{
+    // Fields
+
+    // Information about the location
+    public int ID;
+    public string Name;
+    public string Description;
+    // Location links
+    public Location LocationToNorth;
+    public Location LocationToEast;
+    public Location LocationToSouth;
+    public Location LocationToWest;
+    // Location features
+    public Monster MonsterLivingHere;
+    public Quest QuestAvailableHere;
+
+    //Constructor
+    public Location(int id, string name, string description, Location north = null, Location east = null, Location south = null, Location west = null)
+    {
+        this.ID = id;
+        this.Name = name;
+        this.Description = description;
+        this.LocationToNorth = north;
+        this.LocationToEast = east;
+        this.LocationToSouth = south;
+        this.LocationToWest = west;
+    }
+}
