08c816e60e6bf3c43fa7ceac9f62835a8b8a34a5
JesseBr27 <1105432@hr.nl>
2025-09-10 15:25:28 +0200

    Update Location.cs

--- project a/Location.cs
+++ project a/Location.cs
@@ -14,8 +14,9 @@
     // Location features
     public Monster MonsterLivingHere;
     public Quest QuestAvailableHere;
+    public Location ItemRequiredToEnter;
 
-    //Constructor
+    // Constructor
     public Location(int id, string name, string description, Location north = null, Location east = null, Location south = null, Location west = null)
     {
         this.ID = id;
@@ -25,5 +26,55 @@
         this.LocationToEast = east;
         this.LocationToSouth = south;
         this.LocationToWest = west;
+    }
+
+    // Making a player class to populate in the world
+    public class Player
+    {
+        public Location CurrentLocation;
+
+        public Player(Location start_location)
+        {
+            CurrentLocation = start_location;
+        }
+    }
+
+    public void ShowCompass()
+    {
+        System.Console.WriteLine($"You are at location: {CurrentLocation.Name}");
+        System.Console.WriteLine(CurrentLocation.Description);
+        System.Console.WriteLine("");
+
+        // North compass
+        if (LocationToNorth != null)
+        {
+            Console.WriteLine("    N");
+            Console.WriteLine("    |");
+        }
+        string line = "    |"; // default if no directions allowed
+
+        if (west_Y && east_Y != null)
+        {
+            line = "W---|---E";
+        }
+        else if (west_Y != null)
+        {
+            line = "W---|";
+        }
+        else if (east_Y != null)
+        {
+            line = "    |---E";
+        }
+        Console.WriteLine(line);
+        
+        // South
+        if (south_Y)
+        {
+            Console.WriteLine("    |");
+            Console.WriteLine("    S");
+        }
+
+        // Blank line
+        Console.WriteLine();
     }
 }
